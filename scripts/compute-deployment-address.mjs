2c7feff608ff755c81de761485df05e6994fead0
Shima <shima@shib.army>
2025-09-14 15:13:41 -0400

    feat: compute deployment address

---
+++ scripts/compute-deployment-address.mjs
@@ -0,0 +1,10 @@
+// node scripts/compute-deployment-address.mjs
+
+// yarn add -D ethers
+import { getCreateAddress } from 'ethers';
+
+const from  = '0x2bff9cB1C0e355595130038b56AE705E9BCB8508';
+const nonce = 62n; // BigInt
+
+const addr = getCreateAddress({ from, nonce });
+console.log(addr); // 0x...
